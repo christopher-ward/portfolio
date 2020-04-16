2bd700a35380e4816074d775435d3f8efaa3d09e
Christopher Ward <chrsward90@gmail.com>
2020-04-15 23:48:19 -0700

    current skeleton with about me sample text

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './header';
+import AboutMe from './about-me';
 
 export default class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <div className="container-fluid about"></div>
+        <AboutMe />
         <div className="container-fluid skills"></div>
         <div className="container-fluid applications"></div>
         <div className="container-fluid contact"></div>
